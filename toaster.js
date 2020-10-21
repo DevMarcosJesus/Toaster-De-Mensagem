91e9dbeb47edb75cbf5919ab6109c65bd650624f
Marcos <marcos@evence.com.br>
2020-10-21 09:35:28 -0300

    upload

---
+++ toaster.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(function(){
+        toastr.options = {
+            "timeOut": "4000",
+            "positionClass": "toast-top-center"
+        }
+        toastr.success("teste123");
+    });
+});
